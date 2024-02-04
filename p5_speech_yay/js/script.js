c5508bd6170447fa2bad90c1cc5c11b54dc2879a
Patrick Marler <patmarler@me.com>
2024-02-04 18:53:34 -0500

    added some practice

--- p5_speech_yay/js/script.js
+++ p5_speech_yay/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Title of Project: P5 speech fun project
+Author Name: Patrick Marler
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,6 +8,11 @@ author, and this description to match your project!
 
 "use strict";
 
+let voice = new p5.Speech()
+
+function mousePressed() {
+    voice.speak('she sells sea shells on the seashore');
+}
 
 /**
 Description of preload
