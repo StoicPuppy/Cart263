c051398211443112724f4106b4396cbc28520da6
Patrick Marler <patmarler@me.com>
2024-02-06 14:30:21 -0500

    testing fun

--- p5_speech_yay/js/script.js
+++ p5_speech_yay/js/script.js
@@ -8,11 +8,10 @@ author, and this description to match your project!
 
 "use strict";
 
-let voice = new p5.Speech()
+const speechSynthesizer = new p5.Speech();
 
-function mousePressed() {
-    voice.speak('she sells sea shells on the seashore');
-}
+let showSubtitle = false;
+let toSay = 'when does the fun start ?\n I love pokemon Unite Buzzwole';
 
 /**
 Description of preload
@@ -26,7 +25,18 @@ function preload() {
 Description of setup
 */
 function setup() {
+    createCanvas(500,500);
 
+    // Synthesis settings
+    
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice('Google UK English Female');
+
+    speechSynthesizer.onStart = speechStarted;
+    speechSynthesizer.onEnd = speechEnded;
+
+    console.log(speechSynthesizer.listVoices());
 }
 
 
@@ -34,5 +44,25 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(227,127,111);
 
+    if(showSubtitle){
+        textSize(15);
+        Text(toSay, 100, 100)
+    }
+
+}
+
+function mousePressed() {
+    // Say something
+    
+    speechSynthesizer.speak(toSay);
+}
+
+function speechStarted(){
+    showSubtitle = true;
+}
+
+function speechEnded(){
+    showSubtitle = false;
 }
