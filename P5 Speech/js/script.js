97fb208bb12d8ad3528fb064543c357a2deafbe7
Patrick Marler <patmarler@me.com>
2024-02-06 23:54:58 -0500

    test

--- P5 Speech/js/script.js
+++ P5 Speech/js/script.js
@@ -9,9 +9,12 @@ author, and this description to match your project!
 "use strict";
 
 const speechSynthesizer = new p5.Speech();
+const speechRecognizer = new p5.SpeechRec();
+
+speechRecognizer.continuous = true;
 
 let showSubtitle = false;
-let toSay = 'when does the fun start ?\n I love pokemon Unite Buzzwole';
+let toSay = 'I love pokemon Unite Buzzwole';
 
 /**
 Description of preload
@@ -29,14 +32,16 @@ function setup() {
 
     // Synthesis settings
     
-    speechSynthesizer.setPitch(1);
-    speechSynthesizer.setRate(1);
+    speechSynthesizer.setPitch(0.9);
+    speechSynthesizer.setRate(0.8);
     speechSynthesizer.setVoice('Google UK English Female');
 
     speechSynthesizer.onStart = speechStarted;
     speechSynthesizer.onEnd = speechEnded;
 
     console.log(speechSynthesizer.listVoices());
+
+    speechRecognizer.start();
 }
 
 
@@ -51,6 +56,17 @@ function draw() {
         text(toSay, 100, 100)
     }
 
+}
+
+function ResultCase(){
+    switch(speechRecognizer.resultString){
+        case "Hi":
+            speechSynthesizer.speak('Hello my name is freddy fazbear');
+        break;
+        case "welcome":
+            speechSynthesizer.speak('its me mario');
+    }
+       
 }
 
 function mousePressed() {
