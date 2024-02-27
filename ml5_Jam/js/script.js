cb800b3e4a82f4886e0060ab77f1393a6f123408
Patrick Marler <patmarler@me.com>
2024-02-26 21:59:28 -0500

    added instructions

--- ml5_Jam/js/script.js
+++ ml5_Jam/js/script.js
@@ -1,9 +1,21 @@
 /**
-Title of Project
-Author Name
+Oufit rating game !
+Patrick Marler
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
+
+1- I need to get the computer to say hi
+2- get the computer to ask for an article of clothing to be shown
+3- user shows article of clothing and computer stores the confidence level.
+4- ask for 2 more articles of clothing
+5- display final rating based on combined confidence level of all three pieces of clothing
+
+extra game***
+1- get user to show cat
+2- try to get cat to fill up the screen as much as possible
+3- big cat
+
 */
 
 "use strict";
