4cbdf74bf26fc603203b620374b79414c38cd6eb
Patrick Marler <patmarler@me.com>
2024-02-26 23:46:11 -0500

    added basic implementation

--- ml5_Jam/js/script.js
+++ ml5_Jam/js/script.js
@@ -26,13 +26,14 @@ let modelName = 'CocoSsd';
 let cocossd;
 let predictions = [];
 let clothesPiece = 'person';
+let clothes1 = 0;
 
 
 /**
 Description of setup
 */
 function setup() {
-    createCanvas(700, 500);
+    createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
     cocossd = ml5.objectDetector('cocossd', {}, function(){
@@ -54,12 +55,12 @@ function gotResults(err, results){
 Description of draw()
 */
 function draw() {
-    if(state === 'loadin'){
+    if(state === 'loading'){
         loading();
     }
     else if(state === 'running'){
         running();
-    } else if (state === oufit){
+    } else if (state === 'found'){
         pieceFound();
     }
 }
@@ -77,5 +78,36 @@ function running() {
     background(255)
     text("let me rate your oufit before you put it on!")
 
+    if(predictions){
+        for (let i = 0; i<predictions.length; i++){
+            let object = predictions[i];
+            text("this "+object.label+" is beautiful. I give it a "+object.confidence.toFixed(2) *100+"%", 10, 15*i+50);
+            //console.log(object.confidence.toFixed(2) *100);
+            if(object.label === clothesPiece){
+                // add countdown
+                console.log(object.confidence.toFixed(2) *100)
+                clothes1 = object.confidence.toFixed(1) *10;
+                state = 'found';
+            }
+        }
+    }
+
+    //computer asks user to show an article of clothing
+    //after three second counter we take the confidence level
     
+
+
+    //show me an object: I chose which object first
+    //once it detects the object I want, start countdown.
+    //save confidence level.
+
+
+}
+
+function mousePressed(){
+    console.log("send me funny cat videos");
+}
+
+function pieceFound(){
+    console.log(clothes1);
 }
